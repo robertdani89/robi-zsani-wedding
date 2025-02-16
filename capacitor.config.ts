8f15a663db0436e60f1c1eb26c8c61190c56e60f
robert.dani <robert.dani@leopoly.com>
2025-02-16 19:41:02 +0100

    basic setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'robi.zsani.wedding',
+  appName: 'robi-zsani-wedding',
+  webDir: 'dist/robi-zsani-wedding/browser',
+};
+
+export default config;
