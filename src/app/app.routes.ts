1bc502cedaae7e4e83a98db642d2901d6b05fb19
robert.dani <robert.dani@leopoly.com>
2025-02-18 21:24:26 +0100

    app background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: WelcomeComponent,
+    path: '',
+  },
+];
